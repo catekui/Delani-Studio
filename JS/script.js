066b2f82313b21ab7b1ec570812e0d10829873d8
catekui <catherine.wanjiku@student.moringaschool.com>
2022-03-13 01:18:35 +0300

    add hover effects in class portfolio

--- JS/script.js
+++ JS/script.js
@@ -15,4 +15,32 @@ $(document).ready(function(){
     $('.port').hover(function(){
         $(this).find('#project1').toggle(500);
     });
+    $('#project2').hide();
+    $('.port').hover(function(){
+        $(this).find('#project2').toggle(500);
+    });
+    $('#project3').hide();
+    $('.port').hover(function(){
+        $(this).find('#project3').toggle(500);
+    });
+    $('#project4').hide();
+    $('.port').hover(function(){
+        $(this).find('#project4').toggle(500);
+    });
+    $('#project5').hide();
+    $('.port').hover(function(){
+        $(this).find('#project5').toggle(500);
+    });
+    $('#project6').hide();
+    $('.port').hover(function(){
+        $(this).find('#project6').toggle(500);
+    });
+    $('#project7').hide();
+    $('.port').hover(function(){
+        $(this).find('#project7').toggle(500);
+    });
+    $('#project8').hide();
+    $('.port').hover(function(){
+        $(this).find('#project8').toggle(500);
+    });
 });
